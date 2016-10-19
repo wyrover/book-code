b5b70a3edacb7a4f134edc8ed51e3bfd801dd9c8
wyrover <wyrover@gmail.com>
2016-10-19 17:36:31 +0800

    * gradle.properties

--- android-examples/Test001/app/src/main/java/com/example/rover/test001/MainActivity.java
+++ android-examples/Test001/app/src/main/java/com/example/rover/test001/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.rover.test001;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+// 日志
+import android.util.Log;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("HelloWorldActivity", "onCreate execute");
     }
 }
